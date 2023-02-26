0892e7cca816d24fe7a361b1e0693a88c0c34182
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-02-26 16:02:11 +0200

    adding active age check

--- trip.js
+++ trip.js
@@ -1,11 +1,12 @@
 window.onload = function () {
   document.getElementById("birthday").addEventListener("change", function () {
     var DOB = this.value;
-    var dateEntered = new Date(DOB);
+    var birth_date = new Date(DOB);
     var current_date = new Date()
-    var birth_year = current_date.getFullYear() - dateEntered.getFullYear();
-    var birth_month = dateEntered.getMonth() - current_date.getMonth();
+    var birth_year = current_date.getFullYear() - birth_date.getFullYear();
+    var birth_month = birth_date.getMonth() - current_date.getMonth();
     var age;
+    var ageoutput = "it is prime";
 
     if (birth_month > 0) {
       age = birth_year - 1;
@@ -16,17 +17,20 @@ window.onload = function () {
 
     function isPrime(n) {
       if (n <= 1) {
+        ageoutput = "it is not prime"
         return false;
       }
       for (let i = 2; i < n; i++) {
         if (n % i === 0) {
+          ageoutput = "it is not prime"
           return false;
         }
       }
       return true;
     }
 
-    console.log(age);
-    console.log(isPrime(age))
+    var output_age = document.querySelector(".output1");
+    output_age.textContent = "Your age is " + age + ", " + ageoutput;
+
   });
 }
